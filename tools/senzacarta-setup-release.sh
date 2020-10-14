34de2501c71b6f834fe57fe914966741cea0d350
hexavik <vikrant.ap@econote.in>
2020-10-14 20:05:33 +0530

    added missing source parameter

--- tools/senzacarta-setup-release.sh
+++ tools/senzacarta-setup-release.sh
@@ -4,34 +4,101 @@
 #
 # Copyright (C) 2020 SenzaCarta
 
-. sources/meta-senzacarta/tools/setup-utils.sh
+. sources/meta-imx/tools/setup-utils.sh
 
 CWD=`pwd`
 PROGNAME="setup-environment"
 exit_message ()
 {
-	echo "To return to this build environment later please run:"
-	echo "    source setup-environment build-sc"
+   echo "To return to this build environment later please run:"
+   echo "    source setup-environment <build_dir>"
+
 }
 
-usage ()
+usage()
 {
-	echo -e "\nUsage: source senzacarta-setup-release.sh
-	Optional parameters: [-b build-dir] [-h]"
-	echo "
-		* [-b build-dir]: Build Directory, if unspecified script uses 'build' as output directory
-		* [-h]: help
-	"
+    echo -e "\nUsage: source imx-setup-release.sh
+    Optional parameters: [-b build-dir] [-h]"
+echo "
+    * [-b build-dir]: Build directory, if unspecified script uses 'build' as output directory
+    * [-h]: help
+"
 }
 
-clean_up ()
+
+clean_up()
 {
-	unset CWD BUILD_DIR FSLDISTRO
-	unset fsl_setup_help fsl_setup_error fsl_setup_flag
-	unset usage clean_up
-	unset ARM_DIR META_FSL_BSP_RELEASE
-	exit_message clean_up
+
+    unset CWD BUILD_DIR FSLDISTRO
+    unset fsl_setup_help fsl_setup_error fsl_setup_flag
+    unset usage clean_up
+    unset ARM_DIR META_FSL_BSP_RELEASE
+    exit_message clean_up
 }
+
+# get command line options
+OLD_OPTIND=$OPTIND
+unset FSLDISTRO
+
+while getopts "k:r:t:b:e:gh" fsl_setup_flag
+do
+    case $fsl_setup_flag in
+        b) BUILD_DIR="$OPTARG";
+           echo -e "\n Build directory is " $BUILD_DIR
+           ;;
+        h) fsl_setup_help='true';
+           ;;
+        \?) fsl_setup_error='true';
+           ;;
+    esac
+done
+shift $((OPTIND-1))
+if [ $# -ne 0 ]; then
+    fsl_setup_error=true
+    echo -e "Invalid command line ending: '$@'"
+fi
+OPTIND=$OLD_OPTIND
+if test $fsl_setup_help; then
+    usage && clean_up && return 1
+elif test $fsl_setup_error; then
+    clean_up && return 1
+fi
+
+
+if [ -z "$DISTRO" ]; then
+    if [ -z "$FSLDISTRO" ]; then
+        FSLDISTRO='fsl-imx-xwayland'
+    fi
+else
+    FSLDISTRO="$DISTRO"
+fi
+
+if [ -z "$BUILD_DIR" ]; then
+    BUILD_DIR='build'
+fi
+
+if [ -z "$MACHINE" ]; then
+    echo setting to default machine
+    MACHINE='imx6qpsabresd'
+fi
+
+case $MACHINE in
+imx8*)
+    case $DISTRO in
+    *wayland)
+        : ok
+        ;;
+    *)
+        echo -e "\n ERROR - Only Wayland distros are supported for i.MX 8 or i.MX 8M"
+        echo -e "\n"
+        return 1
+        ;;
+    esac
+    ;;
+*)
+    : ok
+    ;;
+esac
 
 # Cleanup previous meta-freescale/EULA overrides
 cd $CWD/sources/meta-freescale
@@ -46,7 +113,14 @@ fi
 cd -
 
 # Override the click-through in meta-freescale/EULA
-FSL_EULA_FILE=$CWD/sources/meta-senzacarta/EULA
+FSL_EULA_FILE=$CWD/sources/meta-imx/EULA.txt
+
+# Set up the basic yocto environment
+if [ -z "$DISTRO" ]; then
+   DISTRO=$FSLDISTRO MACHINE=$MACHINE . ./$PROGNAME $BUILD_DIR
+else
+   MACHINE=$MACHINE . ./$PROGNAME $BUILD_DIR
+fi
 
 # Point to the current directory since the last command changed the directory to $BUILD_DIR
 BUILD_DIR=.
@@ -65,13 +139,43 @@ else
     cp $BUILD_DIR/conf/local.conf.org $BUILD_DIR/conf/local.conf
 fi
 
+echo >> conf/local.conf
+echo "# Switch to Debian packaging and include package-management in the image" >> conf/local.conf
+echo "PACKAGE_CLASSES = \"package_deb\"" >> conf/local.conf
+echo "EXTRA_IMAGE_FEATURES += \"package-management\"" >> conf/local.conf
+
 if [ ! -e $BUILD_DIR/conf/bblayers.conf.org ]; then
     cp $BUILD_DIR/conf/bblayers.conf $BUILD_DIR/conf/bblayers.conf.org
 else
     cp $BUILD_DIR/conf/bblayers.conf.org $BUILD_DIR/conf/bblayers.conf
 fi
 
-META_FSL_BSP_RELEASE="${CWD}/sources/meta-senzacarta"
+
+META_FSL_BSP_RELEASE="${CWD}/sources/meta-imx/meta-bsp"
+
+echo "" >> $BUILD_DIR/conf/bblayers.conf
+echo "# i.MX Yocto Project Release layers" >> $BUILD_DIR/conf/bblayers.conf
+hook_in_layer meta-imx/meta-bsp
+hook_in_layer meta-imx/meta-sdk
+hook_in_layer meta-imx/meta-ml
+
+echo "" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-browser\"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-rust\"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-gnome\"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-networking\"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-filesystems\"" >> $BUILD_DIR/conf/bblayers.conf
+
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-qt5\"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-python2\"" >> $BUILD_DIR/conf/bblayers.conf
+
+if [ -d ../sources/meta-ivi ]; then
+    echo -e "\n## Genivi layers" >> $BUILD_DIR/conf/bblayers.conf
+    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-gplv2\"" >> $BUILD_DIR/conf/bblayers.conf
+    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-ivi/meta-ivi\"" >> $BUILD_DIR/conf/bblayers.conf
+    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-ivi/meta-ivi-bsp\"" >> $BUILD_DIR/conf/bblayers.conf
+    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-ivi/meta-ivi-test\"" >> $BUILD_DIR/conf/bblayers.conf
+fi
 
 echo "" >> $BUILD_DIR/conf/bblayers.conf
 echo "# SenzaCarta Yocto Project Release layer" >> $BUILD_DIR/conf/bblayers.conf
@@ -79,6 +183,14 @@ echo "BBLAYERS += \"\${BSPDIR}/sources/meta-senzacarta\"" >> $BUILD_DIR/conf/bbl
 
 echo BSPDIR=$BSPDIR
 echo BUILD_DIR=$BUILD_DIR
+
+# Support integrating community meta-freescale instead of meta-fsl-arm
+if [ -d ../sources/meta-freescale ]; then
+    echo meta-freescale directory found
+    # Change settings according to environment
+    sed -e "s,meta-fsl-arm\s,meta-freescale ,g" -i conf/bblayers.conf
+    sed -e "s,\$.BSPDIR./sources/meta-fsl-arm-extra\s,,g" -i conf/bblayers.conf
+fi
 
 cd  $BUILD_DIR
 clean_up
